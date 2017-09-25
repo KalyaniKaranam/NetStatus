e6b42c3f92c74d3e7c79c7343590c80894dad739
KalyaniKaranam <30898080+KalyaniKaranam@users.noreply.github.com>
2017-09-25 17:33:59 +0530

    Add files via upload

---
+++ NetStatus/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace NetStatus
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = CrossConnectivity.Current.IsConnected ? (Page)new NetworkPage(): new NoNetworkPage();
+           
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            CrossConnectivity.Current.ConnectivityChanged += Current_ConnectivityChanged;
+		}
+
+        private void Current_ConnectivityChanged(object sender, Plugin.Connectivity.Abstractions.ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+            if (e.IsConnected && currentPage != typeof(NetworkPage))
+                this.MainPage = new NetworkPage();
+            else if (!e.IsConnected && currentPage != typeof(NoNetworkPage))
+                this.MainPage = new NoNetworkPage();
+        }
+
+        protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ NetStatus/NetworkPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Connectivity.Abstractions;
+
+namespace NetStatus
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NetworkPage : ContentPage
+	{
+		public NetworkPage ()
+		{
+			InitializeComponent ();
+		}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            lblConnectiondetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+            CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if(CrossConnectivity.Current!=null&&CrossConnectivity.Current.ConnectionTypes!=null)
+            {
+                var ConnectionType = CrossConnectivity.Current.ConnectionTypes;
+                lblConnectiondetails.Text = ConnectionType.ToString();
+            }
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (CrossConnectivity.Current != null)
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+        
+    }
+}
